460131c882b93ab4e62cf8c0e8a5df6184492f9a
Nont Banditwong <nontster@gmail.com>
2026-07-26 00:40:44 +0700

    fix: update SET100 tickers list with exact 100 constituents from official SET API

--- src/set100_tickers.py
+++ src/set100_tickers.py
@@ -1,14 +1,16 @@
 from typing import List
 
 # List of Stock Exchange of Thailand (SET 100 index) tickers
+# Verified live from https://www.set.or.th/th/market/index/set100/overview
 SET100_TICKERS: List[str] = [
-    "AAV", "ADVANC", "AMATA", "AOT", "AP", "AWC", "BAM", "BANPU", "BBL", "BCH",
-    "BCP", "BCPG", "BDMS", "BEM", "BGRIM", "BH", "BLA", "BPP", "BTS", "CBG",
-    "CENTEL", "CHG", "CK", "CKP", "COM7", "CPALL", "CPAXT", "CPF", "CPN", "CRC",
-    "DELTA", "EA", "EGCO", "ERW", "GLOBAL", "GPSC", "GULF", "GUNKUL", "HANA", "HMPRO",
-    "ICHI", "INTUCH", "IVL", "JMT", "JMART", "KBANK", "KCE", "KKP", "KTB", "KTC",
-    "LH", "M", "MAJOR", "MBK", "MEGA", "MINT", "MTC", "OR", "ORI", "OSP",
-    "PLANB", "PRM", "PTG", "PTT", "PTTEP", "PTTGC", "QH", "RATCH", "SAWAD", "SCB",
-    "SCC", "SCGP", "SIRI", "SPALI", "SPRC", "STA", "STEC", "STGT", "TCAP", "THANI",
-    "TISCO", "TLI", "TOP", "TRUE", "TTB", "TU", "VGI", "WHA", "WHAUP", "WSN"
+    "AAV", "ADVANC", "AEONTS", "AMATA", "AOT", "AP", "AURA", "AWC", "BA", "BAM",
+    "BANPU", "BBL", "BCH", "BCP", "BCPG", "BDMS", "BEM", "BGRIM", "BH", "BJC",
+    "BLA", "BTG", "BTS", "CBG", "CCET", "CENTEL", "CHG", "CK", "COM7", "CPALL",
+    "CPF", "CPN", "CRC", "DELTA", "DOHOME", "EA", "EGCO", "ERW", "GFPT", "GLOBAL",
+    "GPSC", "GULF", "GUNKUL", "HANA", "HMPRO", "ICHI", "IRPC", "IVL", "JMART", "JMT",
+    "KBANK", "KCE", "KKP", "KTB", "KTC", "LH", "M", "MEGA", "MINT", "MOSHI",
+    "MRDIYT", "MTC", "OR", "OSP", "PLANB", "PR9", "PRM", "PTG", "PTT", "PTTEP",
+    "PTTGC", "QH", "RATCH", "RCL", "SAWAD", "SCB", "SCC", "SCGP", "SIRI", "SPALI",
+    "SPRC", "STA", "STECON", "STGT", "TASCO", "TCAP", "TFG", "THAI", "THCOM", "TIDLOR",
+    "TISCO", "TLI", "TOA", "TOP", "TRUE", "TTB", "TU", "VGI", "WHA", "WHAUP"
 ]
